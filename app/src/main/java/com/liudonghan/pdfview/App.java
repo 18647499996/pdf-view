aa95d72de132c9b0ce2f67157f65dae8bf22c1ed
刘冬涵 <309147563@qq.com>
2023-05-06 11:10:38 +0800

    init project pdf view

---
+++ app/src/main/java/com/liudonghan/pdfview/App.java
@@ -0,0 +1,18 @@
+package com.liudonghan.pdfview;
+
+import android.app.Application;
+
+/**
+ * Description：
+ *
+ * @author Created by: Li_Min
+ * Time:5/6/23
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/com/liudonghan/pdfview/MainActivity.java
@@ -0,0 +1,61 @@
+package com.liudonghan.pdfview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.liudonghan.pdf.ADPdfView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ADPdfView adPdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adPdfView = findViewById(R.id.pdf);
+        File file = new File("/storage/emulated/0/Download/WeiXin/dianzi.pdf");
+        Uri uri = Uri.fromFile(file);
+        Log.i("Mac_Liu：", uri.toString());
+        adPdfView.loadFilePath(uri.toString());
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/pdf");
+
+                //指定类型
+//            var types = arrayOf(pdf)
+//            intent.putExtra(Intent.EXTRA_MIME_TYPES,types)
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 10);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case 10:
+                if (null == data.getData()) {
+                    Log.e(MainActivity.class.getSimpleName(), "onActivityResult: 没有加载到 文件");
+                    return;
+                }
+                Log.i("文件Uri：",data.getData().toString());
+                Uri returnUri = data.getData();
+                adPdfView.loadFilePath(returnUri.toString());
+                break;
+        }
+    }
+}
